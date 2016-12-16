bb08bf8cdce57493562e5de1c5336e289c629ea0
HimariO <dsfhe49854@gmail.com>
2016-12-16 19:12:09 +0800

    update make_coco_dataset script to generator object for better reusable.

--- make_coco_dataset.py
+++ make_coco_dataset.py
@@ -8,48 +8,82 @@ import json
 import math
 from termcolor import colored
 
-start_img = 1000
-end_img = 2000
 
-annotation_path = './annotations/captions_train2014.json'
-image_path = './train2014/'
-annotation_result_path = './data/annotations.pickle'
-name_tag = str(int(start_img / 1000)) + 'k_' + str(math.ceil(end_img / 1000)) + 'k'
-feat_path = './data/feats' + name_tag + '.npy'
-anno = None
+class CocoDataSet(object):
 
-with open(annotation_path, 'r') as json_file:
-    anno = json.loads(json_file.read())
+    def __init__(self,
+                 batch_size=1000,
+                 annotation_path='./annotations/captions_train2014.json',
+                 image_path='./train2014/'):
+        self.annotation_path = annotation_path
+        self.image_path = image_path
+        # self.annotation_result_path = './data/annotations.pickle'
+        self.current_id = 0
+        self.batch_size = batch_size
 
-image_paths = list(map(lambda img: image_path + img['file_name'], anno['images']))
-image_ids = list(map(lambda img: img['id'], anno['images']))
-image_number = end_img - start_img
-# image_number = 100
+    def __iter__(self):
+        with open(self.annotation_path, 'r') as json_file:
+            self.anno = json.loads(json_file.read())
+            self.current_id = 0
 
-feats = np.zeros([image_number] + [512, 14, 14])
-ids = np.zeros([image_number])
+            self.image_num = len(self.anno['images'])
+            self.image_paths = list(map(lambda img: self.image_path + img['file_name'], self.anno['images']))
+            self.image_ids = list(map(lambda img: img['id'], self.anno['images']))
 
-with tf.Session() as sess:
-    vgg = Vgg19(vgg19_npy_path='./VGG/vgg19.npy')
-    image_holder = tf.placeholder('float', [1, 224, 224, 3])
-    vgg.build(image_holder)
+        return self
 
-    for ind, path in zip(range(start_img, end_img), image_paths[start_img: end_img]):
-        print('Loading: %d' % (ind), colored(path, color='yellow'))
+    def __next__(self):
+        if self.current_id < self.image_num:
+            end_num = self.current_id + self.batch_size if self.current_id + self.batch_size < self.image_num else self.image_num
+            result = self.GetFeats(start_img=self.current_id, end_img=end_num)
+            self.current_id += self.batch_size
+            return result
+        else:
+            raise StopIteration
 
-        try:
-            img = utils.load_image(path)
-            batch = img.reshape((1, 224, 224, 3))
+    def GetFeats(start_img=0, end_img=2000, save_npy=False):
+        """
+        Get Feature from vgg19 with more than one image input.
+        if you dont want to save features as npy file, you can iter through this object directal.
+        ex: [batch_feat_id for batch_feature_id in CocoDataSet]
+        """
 
-            # get features of shape [num of img, 14, 14, 512]
-            feature5_3 = sess.run([vgg.conv5_3], feed_dict={image_holder: batch})
-            feats[ind - start_img] = np.reshape(feature5_3[0], [512, 14, 14])
-            ids[ind - start_img] = image_ids[ind]
-            del img, batch
-        except:
-            # some image are not rgb format, so can't be reshape.
-            ids[ind - start_img] = -1
+        # don't need this for now, since i make this a function.
+        # start_img = 1000
+        # end_img = 2000
 
-print('Start Saving...')
-np.save('./data/IDs' + name_tag + '.npy', ids)
-np.save(feat_path, feats)
+        name_tag = str(int(start_img / 1000)) + 'k_' + str(math.ceil(end_img / 1000)) + 'k'
+        feat_path = './data/feats' + name_tag + '.npy'
+        image_number = end_img - start_img
+        # image_number = 100
+
+        feats = np.zeros([image_number] + [512, 14, 14])
+        ids = np.zeros([image_number])
+
+        with tf.Session() as sess:
+            vgg = Vgg19(vgg19_npy_path='./VGG/vgg19.npy')
+            image_holder = tf.placeholder('float', [1, 224, 224, 3])
+            vgg.build(image_holder)
+
+            for ind, path in zip(range(start_img, end_img), self.image_paths[start_img: end_img]):
+                print('Loading: %d' % (ind), colored(path, color='yellow'))
+
+                try:
+                    img = utils.load_image(path)digran
+                    batch = img.reshape((1, 224, 224, 3))
+
+                    # get features of shape [num of img, 14, 14, 512]
+                    feature5_3 = sess.run([vgg.conv5_3], feed_dict={image_holder: batch})
+                    feats[ind - start_img] = np.reshape(feature5_3[0], [512, 14, 14])
+                    ids[ind - start_img] = self.image_ids[ind]
+                    del img, batch
+                except:
+                    # some image are not rgb format, so can't be reshape.
+                    ids[ind - start_img] = -1
+
+        if save_npy:
+            print('Start Saving...')
+            np.save('./data/IDs' + name_tag + '.npy', ids)
+            np.save(feat_path, feats)
+
+        return (feats, ids)
